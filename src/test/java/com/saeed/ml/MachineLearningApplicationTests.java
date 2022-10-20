3568e8d0e6c666ec9944533e693c1c91f4eeb961
SaeedSatari <saeedsatari93@gmail.com>
2022-10-20 15:33:25 +0330

    initial commit

---
+++ src/test/java/com/saeed/ml/MachineLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saeed.ml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
